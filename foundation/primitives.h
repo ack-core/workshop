e5cb69805b07f4b436272ad91c92d0d67d740025
kas.desktop <kas.shaman@gmail.com>
2020-08-09 22:30:37 +0300

    [#] gears + orbit camera controller

--- foundation/primitives.h
+++ foundation/primitives.h
@@ -4,12 +4,13 @@
 #include "math.h"
 #include "rendering/interfaces.h"
 
-class Primitives {
-public:
-    Primitives(const std::shared_ptr<foundation::RenderingInterface> &rendering) : _rendering(rendering) {}
-    
-    void drawLine(const math::vector3f &p1, const math::vector3f &p2, const math::color &rgba) {
-        static const char *lineShader = R"(
+namespace gears {
+    class Primitives {
+    public:
+        Primitives(const std::shared_ptr<foundation::RenderingInterface> &rendering) : _rendering(rendering) {}
+
+        void drawLine(const math::vector3f &p1, const math::vector3f &p2, const math::color &rgba) {
+            static const char *lineShader = R"(
             const {
                 position[2] : float4
                 color : float4
@@ -21,30 +22,30 @@ public:
                 output_color = color;
             }
         )";
-        
-        struct {
-            math::vector4f position1;
-            math::vector4f position2;
-            math::vector4f color;
+
+            struct {
+                math::vector4f position1;
+                math::vector4f position2;
+                math::vector4f color;
+            }
+            lineData{
+                {p1, 1},
+                {p2, 1},
+                rgba
+            };
+
+            if (_lineShader == nullptr) {
+                _lineShader = _rendering->createShader("primitives_line", lineShader, {
+                    {"ID", foundation::RenderingShaderInputFormat::VERTEX_ID}
+                    });
+            }
+
+            _rendering->applyShader(_lineShader, &lineData);
+            _rendering->drawGeometry(2, foundation::RenderingTopology::LINES);
         }
-        lineData {
-            {p1, 1},
-            {p2, 1},
-            rgba
-        };
-        
-        if (_lineShader == nullptr) {
-            _lineShader = _rendering->createShader("primitives_line", lineShader, {
-                {"ID", foundation::RenderingShaderInputFormat::VERTEX_ID}
-            });
-        }
-        
-        _rendering->applyShader(_lineShader, &lineData);
-        _rendering->drawGeometry(2, foundation::RenderingTopology::LINES);
-    }
-    
-    inline void drawCircleXZ(const math::vector3f &position, float radius, const math::color &rgba) {
-        static const char *circleShader = R"(
+
+        inline void drawCircleXZ(const math::vector3f &position, float radius, const math::color &rgba) {
+            static const char *circleShader = R"(
             const {
                 position_radius : float4
                 color : float4
@@ -59,41 +60,41 @@ public:
                 output_color = color;
             }
         )";
-        
-        struct {
-            math::vector4f positionRadius;
-            math::vector4f color;
+
+            struct {
+                math::vector4f positionRadius;
+                math::vector4f color;
+            }
+            circleData{
+                {position, radius},
+                rgba
+            };
+
+            if (_circleShader == nullptr) {
+                _circleShader = _rendering->createShader("primitives_circle", circleShader, {
+                    {"ID", foundation::RenderingShaderInputFormat::VERTEX_ID}
+                    });
+            }
+
+            _rendering->applyShader(_circleShader, &circleData);
+            _rendering->drawGeometry(37, foundation::RenderingTopology::LINESTRIP);
         }
-        circleData {
-            {position, radius},
-            rgba
-        };
-        
-        if (_circleShader == nullptr) {
-            _circleShader = _rendering->createShader("primitives_circle", circleShader, {
-                {"ID", foundation::RenderingShaderInputFormat::VERTEX_ID}
-            });
+
+        void drawCylinderXZ(const math::vector3f &position, float radius, float height, const math::color &rgba) {
+
         }
-        
-        _rendering->applyShader(_circleShader, &circleData);
-        _rendering->drawGeometry(37, foundation::RenderingTopology::LINESTRIP);
-    }
-    
-    void drawCylinderXZ(const math::vector3f &position, float radius, float height, const math::color &rgba) {
-        
-    }
-    
-    void drawCube(const math::vector3f &p1, const math::vector3f &p2, const math::color &rgba) {
-        
-    }
-    
-    void drawAxis() {
-        static const char *axisShader = R"(
+
+        void drawCube(const math::vector3f &p1, const math::vector3f &p2, const math::color &rgba) {
+
+        }
+
+        void drawAxis() {
+            static const char *axisShader = R"(
             fixed {
                 coords[6] : float4 = 
-                    [0.0, 0.0, 0.0, 1.0][100.0, 0.0, 0.0, 1.0]
-                    [0.0, 0.0, 0.0, 1.0][0.0, 100.0, 0.0, 1.0]
-                    [0.0, 0.0, 0.0, 1.0][0.0, 0.0, 100.0, 1.0]
+                    [0.0, 0.0, 0.0, 1.0][1000.0, 0.0, 0.0, 1.0]
+                    [0.0, 0.0, 0.0, 1.0][0.0, 1000.0, 0.0, 1.0]
+                    [0.0, 0.0, 0.0, 1.0][0.0, 0.0, 1000.0, 1.0]
                 colors[3] : float4 = 
                     [1.0, 0.0, 0.0, 1.0]
                     [0.0, 1.0, 0.0, 1.0]
@@ -111,19 +112,20 @@ public:
             }
         )";
 
-        if (_axisShader == nullptr) {
-            _axisShader = _rendering->createShader("primitives_axis", axisShader, {
-                {"ID", foundation::RenderingShaderInputFormat::VERTEX_ID}
-            });
+            if (_axisShader == nullptr) {
+                _axisShader = _rendering->createShader("primitives_axis", axisShader, {
+                    {"ID", foundation::RenderingShaderInputFormat::VERTEX_ID}
+                    });
+            }
+
+            _rendering->applyShader(_axisShader);
+            _rendering->drawGeometry(6, foundation::RenderingTopology::LINES);
         }
 
-        _rendering->applyShader(_axisShader);
-        _rendering->drawGeometry(6, foundation::RenderingTopology::LINES);
-    }
-    
-protected:
-    std::shared_ptr<foundation::RenderingInterface> _rendering;
-    std::shared_ptr<foundation::RenderingShader> _axisShader;
-    std::shared_ptr<foundation::RenderingShader> _lineShader;
-    std::shared_ptr<foundation::RenderingShader> _circleShader;
-};
+    protected:
+        std::shared_ptr<foundation::RenderingInterface> _rendering;
+        std::shared_ptr<foundation::RenderingShader> _axisShader;
+        std::shared_ptr<foundation::RenderingShader> _lineShader;
+        std::shared_ptr<foundation::RenderingShader> _circleShader;
+    };
+}
