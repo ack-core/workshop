e53e5bc09297dbeffb57aa4e01047d9a8cfd06e3
kas <kas.shaman@outlook.com>
2023-09-10 00:07:07 +0300

    [#] vox from png

--- tools/png2pallete.py
+++ tools/png2pallete.py
@@ -4,9 +4,12 @@ from __future__ import annotations
 Tool to correct a png in accordance with palette
 """
 
-import argparse
 import png
+import argparse
+
 from array import array
+
+PALETTE_INDEX_MAX = 30
 
 class Palette:
     def __init__(self, img: array):
@@ -14,40 +17,71 @@ class Palette:
         pass
 
     def get_color(self, index: int, brightness: int):
-        index = min(max(index, 0), 30)
+        index = min(max(index, 0), PALETTE_INDEX_MAX)
         brightness = min(max(brightness, 0), 255)
         offset = (8 + index * 8 + brightness // 32) * 4
         return self._data[offset:offset + 4]
 
 def main(file: str, palette: str, material: int, mask: str):
-    file_reader = png.Reader(filename=file)
-    file_w, file_h, data, _ = file_reader.asRGBA8()
-    file_img = sum([array('B', e) for e in data], array('B'))
+    if file.endswith(".png"):
+        try:
+            file_reader = png.Reader(filename=file)
+            file_w, file_h, data, _ = file_reader.asRGBA8()
+            file_img = sum([array('B', e) for e in data], array('B'))
+            file_size = file_w * file_h
 
-    palette_reader = png.Reader(filename=palette)
-    palette_img = sum([array('B', e) for e in palette_reader.asRGBA8()[2]], array('B'))
-    palette = Palette(img=palette_img)
+            if palette.endswith(".png"):
+                try:
+                    palette_reader = png.Reader(filename=palette)
+                    palette_w, palette_h, palette_data, _ = palette_reader.asRGBA8()
 
-    material_indexes = None
-    if mask:
-        mask_reader = png.Reader(filename=mask)
-        mask_img = sum([array('B', e) for e in mask_reader.asRGBA8()[2]], array('B'))
-        material_indexes = [mask_img[i * 4] // 8 for i in range(0, file_w * file_h)]
+                    if palette_w * palette_h == 256:
+                        palette_img = sum([array('B', e) for e in palette_data], array('B'))
+                        palette = Palette(img=palette_img)
 
-    for i in range(0, file_w * file_h):
-        pixel_start = i * 4
-        pixel_end = i * 4 + 4
-        color = file_img[pixel_start:pixel_end]
-        brightness = (2 * color[0] + 3 * color[1] + color[2]) // 6
+                        material_indexes = None
+                        if mask:
+                            mask_reader = png.Reader(filename=mask)
+                            mask_data = mask_reader.asRGBA8()[2]
+                            mask_img = sum([array('B', e) for e in mask_data], array('B'))
+                            material_indexes = [mask_img[i * 4] // 8 for i in range(0, file_size)]
 
-        m = material if material else material_indexes[i]
-        color = palette.get_color(m, brightness)
+                        for i in range(0, file_size):
+                            pixel_start = i * 4
+                            pixel_end = i * 4 + 4
+                            color = file_img[pixel_start:pixel_end]
+                            brightness = (2 * color[0] + 3 * color[1] + color[2]) // 6
+                            m = material if material else material_indexes[i]
+                            color = palette.get_color(m, brightness)
+                            file_img[pixel_start:pixel_end] = array('B', color)
 
-        file_img[pixel_start:pixel_end] = array('B', color)
+                        out_path = file.replace(".png", "_corrected.png")
+                        try:
+                            with open(out_path, 'wb') as f:
+                                writer = png.Writer(width=file_w, height=file_h, greyscale=False, alpha=True)
+                                writer.write_array(f, pixels=file_img)
 
-    with open(file.replace(".png", "_corrected.png"), 'wb') as f:
-        writer = png.Writer(width=file_w, height=file_h, greyscale=False, alpha=True)
-        writer.write_array(f, pixels=file_img)
+                        except EnvironmentError:
+                            print("can't open {} for writing".format(out_path))
+
+                    else:
+                        print("{} has incorrect size".format(palette))
+
+                except FileNotFoundError:
+                    print("{} not found".format(palette))
+                except png.Error:
+                    print("{} is invalid".format(palette))
+
+            else:
+                print("{} is not a *.png file".format(palette))
+
+        except FileNotFoundError:
+            print("{} not found".format(file))
+        except png.Error:
+            print("{} is invalid".format(file))
+
+    else:
+        print("{} is not a *.png file".format(file))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Tool to correct a png")
--- tools/png2vox.py
+++ tools/png2vox.py
@@ -4,41 +4,126 @@ from __future__ import annotations
 Tool to create vox file from png image
 """
 
+import png
+import typing
 import argparse
 import struct
-import png
 
 from array import array
 
 class Color:
+    rgba = array('B', [0, 0, 0, 0])
+
     def __init__(self, r: int, g: int, b: int, a: int):
-        self._rgba = array("B", [r, g, b, a])
-        self._hash = struct.unpack("<i", self._rgba)
+        self.rgba = array('B', [r, g, b, a])
+        self._hash = struct.unpack("<i", self.rgba)[0]
 
     def __hash__(self):
         return self._hash
 
     def __eq__(self, other: Color):
-        return self._rgba == other._rgba
+        return self.rgba == other.rgba
 
 class Palette:
     def __init__(self, img: array):
-        self._palette = {}
+        self._color_to_index = {}
+        self._colors = []
+
         for i in range(0, 1024, 4):
-            color = Color(img[i], img[i + 1], img[i + 2], img[i + 3])
-            self._palette[color] = i / 4
+            index = i // 4
+            r, g, b, a = img[i:i + 4]
+            color = Color(r, g, b, a)
+            self._color_to_index[color] = index
+            self._colors.append(color)
 
     def get_color_index(self, color: Color):
-        return self._palette.get(color, 0)
+        return self._color_to_index.get(color, 0)
+
+    def get_colors(self):
+        result = array('B')
+        for c in self._colors:
+            result.frombytes(c.rgba)
+
+        return result.tobytes()
+
+class VoxChunk:
+    def __init__(self, name: bytes, content: bytes):
+        self._name = name
+        self._content_size = 0
+        self._children_size = 0
+        self._children_chunks = []
+        self._content_data = content
+
+    def add_child(self, chunk: VoxChunk):
+        self._children_chunks.append(chunk)
+        self._children_size += chunk._content_size + 12
+
+    def write(self, out: typing.BinaryIO):
+        out.write(self._name)
+        out.write(struct.pack("<i", len(self._content_data)))
+        out.write(struct.pack("<i", self._children_size))
+        out.write(self._content_data)
+
+        for child in self._children_chunks:
+            child.write(out)
+
 
 def main(src: str, palette: str, dst: str):
-    src_reader = png.Reader(filename=src)
-    src_w, src_h, data, _ = src_reader.asRGBA8()
-    src_img = sum([array('B', e) for e in data], array('B'))
+    if src.endswith(".png"):
+        try:
+            src_reader = png.Reader(filename=src)
+            src_w, src_h, data, _ = src_reader.asRGBA8()
+            src_img = sum([array('B', e) for e in data], array('B'))
 
-    palette_reader = png.Reader(filename=palette)
-    palette_img = sum([array('B', e) for e in palette_reader.asRGBA8()[2]], array('B'))
-    palette = Palette(img=palette_img)
+            if palette.endswith(".png"):
+                try:
+                    palette_reader = png.Reader(filename=palette)
+                    palette_w, palette_h, palette_data, _ = palette_reader.asRGBA8()
+
+                    if palette_w * palette_h == 256:
+                        palette_img = sum([array('B', e) for e in palette_data], array('B'))
+                        palette = Palette(img=palette_img)
+
+                        with open(dst, mode="wb") as f:
+                            f.write(b"VOX ")
+                            f.write(struct.pack("<i", 150))
+                            main_chunk = VoxChunk(b"MAIN", b"")
+
+                            pack_chunk = VoxChunk(b"PACK", struct.pack("<i", 1))
+                            main_chunk.add_child(pack_chunk)
+
+                            size_chunk = VoxChunk(b"SIZE", struct.pack("<iii", 1, 1, 1))
+                            main_chunk.add_child(size_chunk)
+
+                            voxels = array('B')
+                            voxels.frombytes(struct.pack("<i", 1))
+                            voxels.fromlist([0, 0, 0, 1])
+
+                            data_chunk = VoxChunk(b"XYZI", voxels.tobytes())
+                            main_chunk.add_child(data_chunk)
+
+                            rgba_chunk = VoxChunk(b"RGBA", palette.get_colors())
+                            main_chunk.add_child(rgba_chunk)
+                            main_chunk.write(f)
+
+                    else:
+                        print("{} has incorrect size".format(palette))
+
+                except FileNotFoundError:
+                    print("{} not found".format(palette))
+                except png.Error:
+                    print("{} is invalid".format(palette))
+
+            else:
+                print("{} is not a *.png file".format(palette))
+
+        except FileNotFoundError:
+            print("{} not found".format(src))
+        except png.Error:
+            print("{} is invalid".format(src))
+
+    else:
+        print("{} is not a *.png file".format(src))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Tool to create vox from png")
