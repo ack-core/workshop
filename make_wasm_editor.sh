975a989555f585433881757e17f9faef2acf2b87
kas <kas.shaman@outlook.com>
2025-01-03 11:47:10 +0300

    emsdk version

--- make_wasm_editor.sh
+++ make_wasm_editor.sh
@@ -2,3 +2,5 @@ EMSDK=/Users/kas/work/2023/emsdk cmake -S . -B ./workshop_wasm -G"Unix Makefiles
 make -C workshop_wasm -s
 
 # /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --ignore-certificate-errors --ignore-urlfetcher-cert-requests --disable-web-security --user-data-dir=/Users/kas/work/tmp
+
+# emsdk: 3.1.47
